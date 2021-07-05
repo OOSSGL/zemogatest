e41c499b4bd36ba514f4ad91e593b3766ce80808
Oscar Garcia <oossgl@gmail.com>
2021-07-05 17:42:37 -0500

    Resftul CRUD complete

---
+++ app.py
@@ -0,0 +1,218 @@
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from werkzeug.exceptions import BadRequest, NotFound
+import os
+
+# Create the application instance
+app = Flask(__name__, template_folder="templates")
+
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://zemoga_test_db:Zem0ga.101@zemoga-test-db.crhpedy9xxto.us-east-1.rds.amazonaws.com/zemoga_test_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Init db
+db = SQLAlchemy(app)
+# Init ma
+ma = Marshmallow(app)
+
+
+# Portfolio Class/Model
+class Portfolio(db.Model):
+    idportfolio = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(255))
+    image_url = db.Column(db.String(255))
+    twitter_user_name = db.Column(db.String(255))
+    title = db.Column(db.String(255))
+    user_id = db.Column(db.String(255))
+    experience_summary = db.Column(db.String(255))
+    last_names = db.Column(db.String(255))
+    names = db.Column(db.String(255))
+    twitter_user_id = db.Column(db.String(255))
+
+    def __init__(
+          self,
+          description,
+          image_url,
+          twitter_user_name,
+          title,
+          user_id,
+          experience_summary,
+          last_names,
+          names,
+          twitter_user_id
+    ):
+        self.description = description
+        self.image_url = image_url
+        self.twitter_user_name = twitter_user_name
+        self.title = title
+        self.user_id = user_id
+        self.experience_summary = experience_summary
+        self.last_names = last_names
+        self.names = names
+        self.twitter_user_id = twitter_user_id
+
+
+# Portfolio Schema
+class PortfolioSchema(ma.Schema):
+    class Meta:
+        fields = (
+            'idportfolio',
+            'description',
+            'image_url',
+            'twitter_user_name',
+            'title',
+            'user_id',
+            'experience_summary',
+            'last_names',
+            'names',
+            'twitter_user_id'
+        )
+
+
+# Init schema
+portfolio_schema = PortfolioSchema()
+portfolios_schema = PortfolioSchema(many=True)
+
+
+# Create a URL route in our application for "/"
+@app.route('/')
+def home():
+    """
+    This function just responds to the browser ULR
+    localhost:5000/
+
+    :return:        the rendered template 'home.html'
+    """
+    return render_template('home.html')
+
+
+# Create a Portfolio
+@app.route('/portfolio', methods=['POST'])
+def add_portfolio():
+    description = None
+    image_url = None
+    twitter_user_name = None
+    title = None
+    user_id = None
+    experience_summary = None
+    last_names = None
+    names = None
+    twitter_user_id = None
+
+    # Check if each field is given and is not null or empty
+    if 'description' not in request.json:
+        raise BadRequest('Field description is required')
+    if not request.json['description']:
+        raise BadRequest('Field description cannot be empty or null')
+    if 'image_url' not in request.json:
+        raise BadRequest('Field image_url is required')
+    if not request.json['image_url']:
+        raise BadRequest('Field image_url cannot be empty or null')
+    if 'twitter_user_name' not in request.json:
+        raise BadRequest('Field twitter_user_name is required')
+    if not request.json['twitter_user_name']:
+        raise BadRequest('Field twitter_user_name cannot be empty or null')
+    if 'title' not in request.json:
+        raise BadRequest('Field title is required')
+    if not request.json['title']:
+        raise BadRequest('Field title cannot be empty or null')
+
+    description = request.json['description']
+    image_url = request.json['image_url']
+    twitter_user_name = request.json['twitter_user_name']
+    title = request.json['title']
+
+    # Those are optional
+    if 'user_id' in request.json and request.json['user_id']:
+        user_id = request.json['user_id']
+    if 'experience_summary' in request.json and request.json['experience_summary']:
+        experience_summary = request.json['experience_summary']
+    if 'last_names' in request.json and request.json['last_names']:
+        last_names = request.json['last_names']
+    if 'names' in request.json and request.json['names']:
+        names = request.json['names']
+    if 'twitter_user_id' in request.json and request.json['twitter_user_id']:
+        twitter_user_id = request.json['twitter_user_id']
+
+    new_portfolio = Portfolio(
+        description,
+        image_url,
+        twitter_user_name,
+        title,
+        user_id,
+        experience_summary,
+        last_names,
+        names,
+        twitter_user_id
+    )
+
+    db.session.add(new_portfolio)
+    db.session.commit()
+
+    return portfolio_schema.jsonify(new_portfolio)
+
+
+# Get All Portfolios
+@app.route('/portfolio', methods=['GET'])
+def get_portfolios():
+    all_portfolios = Portfolio.query.all()
+    result = portfolios_schema.dump(all_portfolios)
+    return jsonify(result)
+
+
+# Get Single Portfolio
+@app.route('/portfolio/<id>', methods=['GET'])
+def get_portfolio(id):
+    portfolio = Portfolio.query.get(id)
+    return portfolio_schema.jsonify(portfolio)
+
+
+# Update a Portfolio
+@app.route('/portfolio/<id>', methods=['PUT'])
+def update_portfolio(id):
+    portfolio = Portfolio.query.get(id)
+    if not portfolio:
+        raise NotFound
+
+    # Check if each field is given and is not null or empty
+    if 'description' in request.json and request.json['description']:
+        portfolio.description = request.json['description']
+    if 'image_url' in request.json and request.json['image_url']:
+        portfolio.image_url = request.json['image_url']
+    if 'twitter_user_name' in request.json and request.json['twitter_user_name']:
+        portfolio.twitter_user_name = request.json['twitter_user_name']
+    if 'title' in request.json and request.json['title']:
+        portfolio.title = request.json['title']
+    if 'user_id' in request.json and request.json['user_id']:
+        portfolio.user_id = request.json['user_id']
+    if 'experience_summary' in request.json and request.json['experience_summary']:
+        portfolio.experience_summary = request.json['experience_summary']
+    if 'last_names' in request.json and request.json['last_names']:
+        portfolio.last_names = request.json['last_names']
+    if 'names' in request.json and request.json['names']:
+        portfolio.names = request.json['names']
+    if 'twitter_user_id' in request.json and request.json['twitter_user_id']:
+        portfolio.twitter_user_id = request.json['twitter_user_id']
+
+    db.session.commit()
+
+    return portfolio_schema.jsonify(portfolio)
+
+
+# Delete Portfolio
+@app.route('/portfolio/<id>', methods=['DELETE'])
+def delete_portfolio(id):
+    portfolio = Portfolio.query.get(id)
+    if not portfolio:
+        raise NotFound
+
+    db.session.delete(portfolio)
+    db.session.commit()
+
+    return portfolio_schema.jsonify(portfolio)
+
+
+# If we're running in stand alone mode, run the application
+if __name__ == '__main__':
+    app.run(debug=True)
